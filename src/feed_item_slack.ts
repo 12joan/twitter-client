1e75154620ad9a95ee3ce28fded717cd6196b5e3
Ognian Tenchev <drJeckyll@users.noreply.github.com>
2023-07-05 17:11:16 +0300

    Create feed_item_slack.ts

---
+++ src/feed_item_slack.ts
@@ -0,0 +1,15 @@
+const id = tweet.rest_id;
+const url = `https://twitter.com/${username}/status/${id}`;
+const date = tweet.legacy.created_at;
+const text = tweet.legacy.full_text;
+const mediaUrls = tweet.legacy.entities.media?.map((media) => media.media_url_https) ?? [];
+
+feed.item({
+    title: url,
+    url: url,
+    date,
+    description: [
+	text,
+        ...mediaUrls.map((url: string) => url),
+    ].join('\n'),
+});
