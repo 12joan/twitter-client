8f3d39b00e26cb32105b7c7dbcc1ba82f007bc77
Joe Anderson <joe@anderbell.studio>
2023-07-09 08:59:30 +0100

    Update src/twitter.ts

--- src/twitter.ts
+++ src/twitter.ts
@@ -130,7 +130,7 @@ const withCache = async <T, U>(
     }
   }
   // Refresh cache and retry callback
-  const value = await producer(); 
+  const value = await producer();
   await redis.set(key, JSON.stringify(value)); 
   return callback(value); 
 };
